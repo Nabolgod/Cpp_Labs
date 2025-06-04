da135f5b2142795cf52e5df7e7f66a2e75772a26
Danil Lobanov <140906610+Nabolgod@users.noreply.github.com>
2025-06-04 04:10:23 +0300

    Update lab_9_part_1.cpp

--- lab_9_part_1.cpp
+++ lab_9_part_1.cpp
@@ -1,31 +1,101 @@
 #include <iostream>
-#include <ctime>
 #include <string>
-#include <conio.h>
+#include <fstream>
+#include <cstdlib>
 
 using namespace std;
 
+template <typename diff>
+void my_print(string text, diff &variable) {
+	cout << text;
+	cin >> variable;
+}
 
+void processing_fullname(string &file_name) {
+	while (file_name.find('"') != string::npos)
+		file_name.erase(file_name.find('"'), 1);
+}
+
+bool check_bracket(string row) {
+	int balance = 0;
+	for (int i = 0; i < row.length(); i++) {
+		if (row[i] == '(')
+			balance++;
+		else if (row[i] == ')') {
+			balance--;
+			if (balance < 0)
+				return false;
+		}
+	}
+	return balance == 0;
+}
+
+void check_open(ifstream &file) {
+	if (!file.is_open()) {
+		cout << "Не удалось открыть файл!" << endl;
+		exit(EXIT_FAILURE);
+	}
+	cout << "Файл открыт!" << endl;
+}
+
+void check_open(ofstream& file) {
+	if (!file.is_open()) {
+		cout << "Не удалось открыть файл!" << endl;
+		exit(EXIT_FAILURE);
+	}
+	cout << "Файл открыт!" << endl;
+}
+
+void record_result(ifstream &file_read, ofstream &file_write) {
+	string str;
+	int id_row = 1;
+
+	while (!file_read.eof()) {
+		getline(file_read, str);
+		if (str.find(')') != string::npos || str.find('(') != string::npos) {
+			if (check_bracket(str))
+				file_write << id_row << ". True" << endl;
+			else
+				file_write << id_row << ". False" << endl;
+		}
+		else
+			file_write << id_row << ". В строке нет скобочек!" << endl;
+
+		id_row++;
+	}
+}
 
 int main() {
 	setlocale(LC_ALL, "RU");
 
-	// Открытие файла через его полное имя. Запрашиваю данные от пользователя.
-	// Открываю файл на считывание.
+	// Запрашиваем имя файла из которого будем читать данные.
 
+	string fullname;
+	my_print("Введите полный имя файла для чтения: ", fullname);
+	processing_fullname(fullname);
 
-	// Также сразу создаю файл на запись, в который буду помещать результат считывания.
+	ifstream file_read;
+	file_read.open(fullname);
 
-	// Создаю переменную для индексации строк.
-	// Обрабатываю первый файл построчно.
-	// В каждой строке проверяю наличие скобочек ()
-	// Если скобочки есть, то проверяю их на "правильность" и записываю во второй файл true или false
-	// Если в строке нет скобок (пустая строка) , то записываю её как она есть во второй файл.
-	// Вывод в виде: номер_строки - результат
-	
+	check_open(file_read);
 
-	_getch();
+	// Запрашиваем имя файла в который будем записывать результат чтения.
+
+	string fullname_write;
+	my_print("Введите имя файла для записи: ", fullname_write);
+	processing_fullname(fullname_write);
+
+	ofstream file_write;
+	file_write.open(fullname_write + ".txt");
+
+	check_open(file_write);
+
+	// Логика обработки и результат обработки
+
+	record_result(file_read, file_write);
+
+	file_read.close();
+	file_write.close();
+
 	return 0;
 }
-
-// для разбора темы https://purecodecpp.com/archives/2751
