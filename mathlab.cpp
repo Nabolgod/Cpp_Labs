8316026c64b3990f642c0d222aede247a4225290
Danil Lobanov <140906610+Nabolgod@users.noreply.github.com>
2025-11-19 10:38:17 +0300

    Update mathlab.cpp

--- mathlab.cpp
+++ mathlab.cpp
@@ -1,205 +1,138 @@
 #include <iostream>
 #include <string>
-#include <ctime>
-#include <iomanip>
-#include <cmath>
 
 using namespace std;
 
-template <typename T>
-void my_input(T& value, string text) {
-    cout << text;
-    cin >> value;
-}
-
 class MathObject {
 public:
-    virtual ~MathObject() = default;
+	MathObject __add__(MathObject other) {};
 
-    virtual MathObject& operator+(const MathObject& other) const = 0;
-    virtual MathObject& operator-(const MathObject& other) const = 0;
-    virtual MathObject& operator*(const MathObject& other) const = 0;
-    virtual MathObject& operator*(double scalar) const = 0;
+	MathObject __sub__(MathObject other) {};
 
-    virtual std::string toString() const = 0;
+	MathObject __mul__(MathObject other) {};
+
+	MathObject __mul__(int other) {};
+
+	void __str__(int other) {}
 };
 
 class Matrix : public MathObject {
-private:
-    double** data;
-    int rows, cols;
-
-    void allocateMemory() {
-        data = new double* [rows];
-        for (int i = 0; i < rows; ++i) {
-            data[i] = new double[cols];
-            for (int j = 0; j < cols; ++j) {
-                data[i][j] = 0.0;
-            }
-        }
-    }
-
-    void freeMemory() {
-        if (data) {
-            for (int i = 0; i < rows; ++i) {
-                delete[] data[i];
-            }
-            delete[] data;
-        }
-    }
-
 public:
-    ~Matrix() {
-        freeMemory();
-    }
+	Matrix __add__(Matrix other) {
+		return Matrix();
+	}
 
-    Matrix(int r, int c){
-        rows = r;
-        cols = c;
-        allocateMemory();
-    }
+	Matrix __sub__(Matrix other) {
+		return Matrix();
+	}
 
-    Matrix(double** matrixData, int r, int c){
-        rows = r;
-        cols = c;
+	Matrix __mul__(Matrix other) {
+		return Matrix();
+	}
 
-        allocateMemory();
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < cols; ++j) {
-                data[i][j] = matrixData[i][j];
-            }
-        }
-    }
+	Matrix __mul__(int other) {
+		return Matrix();
+	}
 
-    void setValue(int row, int col, double value) {
-        if (row >= 0 && row < rows && col >= 0 && col < cols) {
-            data[row][col] = value;
-        }
-    }
+	void __str__(int other) {
+		cout << "" << endl;
+	}
+};
 
-    double getValue(int row, int col) const {
-        if (row >= 0 && row < rows && col >= 0 && col < cols) {
-            return data[row][col];
-        }
-        return 0.0;
-    }
+class Polynomial : public MathObject {
+public:
+	Polynomial __add__(Matrix other) {
+		return Polynomial();
+	}
 
-    int getRows() const { return rows; }
-    int getCols() const { return cols; }
+	Polynomial __sub__(Matrix other) {
+		return Polynomial();
+	}
 
-    // Оператор сложения
-    Matrix& operator+(const MathObject& other) const override {
-        const Matrix* otherMatrix = dynamic_cast<const Matrix*>(&other);
-        if (!otherMatrix || rows != otherMatrix->rows || cols != otherMatrix->cols) {
-            throw std::invalid_argument("Matrix dimensions must match for addition");
-        }
+	Polynomial __mul__(Matrix other) {
+		return Polynomial();
+	}
 
-        Matrix* result = new Matrix(rows, cols);
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < cols; ++j) {
-                result->setValue(i, j, data[i][j] + otherMatrix->getValue(i, j));
-            }
-        }
-        return *result;
-    }
+	Polynomial __mul__(int other) {
+		return Polynomial();
+	}
 
-    // Оператор вычитания
-    Matrix& operator-(const MathObject& other) const override {
-        const Matrix* otherMatrix = dynamic_cast<const Matrix*>(&other);
-        if (!otherMatrix || rows != otherMatrix->rows || cols != otherMatrix->cols) {
-            throw std::invalid_argument("Matrix dimensions must match for subtraction");
-        }
+	void __str__(int other) {
+		cout << "" << endl;
+	}
+};
 
-        Matrix* result = new Matrix(rows, cols);
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < cols; ++j) {
-                result->setValue(i, j, data[i][j] - otherMatrix->getValue(i, j));
-            }
-        }
-        return *result;
-    }
+class Vector : public MathObject {
+public:
+	Vector __add__(Vector other) {
+		return Vector();
+	}
 
-    // Оператор умножения матриц
-    Matrix& operator*(const MathObject& other) const override {
-        const Matrix* otherMatrix = dynamic_cast<const Matrix*>(&other);
-        if (!otherMatrix || cols != otherMatrix->rows) {
-            throw std::invalid_argument("Matrix dimensions are incompatible for multiplication");
-        }
+	Vector __sub__(Vector other) {
+		return Vector();
+	}
 
-        Matrix* result = new Matrix(rows, otherMatrix->cols);
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < otherMatrix->cols; ++j) {
-                double sum = 0.0;
-                for (int k = 0; k < cols; ++k) {
-                    sum += data[i][k] * otherMatrix->getValue(k, j);
-                }
-                result->setValue(i, j, sum);
-            }
-        }
-        return *result;
-    }
+	Vector __mul__(Vector other) {
+		return Vector();
+	}
 
-    // Умножение на скаляр
-    Matrix& operator*(double scalar) const override {
-        Matrix* result = new Matrix(rows, cols);
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < cols; ++j) {
-                result->setValue(i, j, data[i][j] * scalar);
-            }
-        }
-        return *result;
-    }
+	Vector __mul__(int other) {
+		return Vector();
+	}
 
-    // Строковое представление
-    std::string toString() const override {
-        std::string result = "Matrix " + std::to_string(rows) + "x" + std::to_string(cols) + ":\n";
-        for (int i = 0; i < rows; ++i) {
-            result += "[ ";
-            for (int j = 0; j < cols; ++j) {
-                result += std::to_string(data[i][j]) + " ";
-            }
-            result += "]\n";
-        }
-        return result;
-    }
+	void __str__(int other) {
+		cout << "" << endl;
+	}
+};
+
+class Fraction : public MathObject {
+public:
+	Fraction __add__(Fraction other) {
+		return Fraction();
+	}
+
+	Fraction __sub__(Fraction other) {
+		return Fraction();
+	}
+
+	Fraction __mul__(Fraction other) {
+		return Fraction();
+	}
+
+	Fraction __mul__(int other) {
+		return Fraction();
+	}
+
+	void __str__(int other) {
+		cout << "" << endl;
+	}
+};
+
+class СomplexNumber : public MathObject {
+public:
+	СomplexNumber __add__(СomplexNumber other) {
+		return СomplexNumber();
+	}
+
+	СomplexNumber __sub__(СomplexNumber other) {
+		return СomplexNumber();
+	}
+
+	СomplexNumber __mul__(СomplexNumber other) {
+		return СomplexNumber();
+	}
+
+	СomplexNumber __mul__(int other) {
+		return СomplexNumber();
+	}
+
+	void __str__(int other) {
+		cout << "" << endl;
+	}
 };
 
 int main() {
-    setlocale(LC_ALL, "RU");
-    srand(time(NULL));
 
-    std::cout << "1. Матрицы:\n";
-    double** m1Data = new double* [2];
-    m1Data[0] = new double[2] {1, 2};
-    m1Data[1] = new double[2] {3, 4};
-
-    double** m2Data = new double* [2];
-    m2Data[0] = new double[2] {5, 6};
-    m2Data[1] = new double[2] {7, 8};
-
-    Matrix m1(m1Data, 2, 2);
-    Matrix m2(m2Data, 2, 2);
-
-    Matrix& sumM = dynamic_cast<Matrix&>(m1 + m2);
-    Matrix& productM = dynamic_cast<Matrix&>(m1 * m2);
-    Matrix& scaledM = dynamic_cast<Matrix&>(m1 * 2.5);
-
-    std::cout << "m1:\n" << m1.toString();
-    std::cout << "m2:\n" << m2.toString();
-    std::cout << "m1 + m2:\n" << sumM.toString();
-    std::cout << "m1 * m2:\n" << productM.toString();
-    std::cout << "m1 * 2.5:\n" << scaledM.toString();
-
-    // Освобождение памяти
-    delete& sumM;
-    delete& productM;
-    delete& scaledM;
-    for (int i = 0; i < 2; ++i) {
-        delete[] m1Data[i];
-        delete[] m2Data[i];
-    }
-    delete[] m1Data;
-    delete[] m2Data;
-
-    return 0;
+	
+	return 0;
 }
